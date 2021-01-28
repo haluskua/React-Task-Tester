c4e969973e207b9885f81703aea36990faa2fe4d
haluskua <haluskua.one@gmail.com>
2021-01-28 17:02:14 +1300

    added Header.js componentn

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 
+import Header from './components/Header'
+
 function App() {
   const name = 'Brad'
   const x = false
@@ -6,6 +8,7 @@ function App() {
     <div className="container">
       <h1>Hello from {name}</h1>
       <h1>Hello from {x ? 'yes' : 'No'}</h1>
+      <Header />
     </div>
   );
 }
