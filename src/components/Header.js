1a2513fe0454e32a3aa140d500fd5177cf33a5d6
haluskua <haluskua.one@gmail.com>
2021-01-31 22:15:29 +1300

    changed Add button to bcg color red

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color={showAdd ? 'pink' : 'green'} text={showAdd ? 'close' : 'Add' } onClick={onAdd} />
+      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'close' : 'Add' } onClick={onAdd} />
     </header>
   );
 };
