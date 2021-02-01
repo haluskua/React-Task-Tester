201b80dc1dc216c21f8f4021ca8707e54ab6e45c
haluskua <haluskua.one@gmail.com>
2021-02-01 14:38:19 +1300

    console loggin data from jason server to console terminal

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
-function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Food Shopping",
-      day: "Feb 5th at 2:50pm",
-      reminder: false,
-    },
-  ]);
+const App = () => {
+  const [tasks, setTasks] = useState([]);
   
  const [showAddTask, setShowAddTask] = useState(false)
+
+useEffect(() => {
+  const fetchTasks = async () =>{
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    
+    console.log(data)
+  } 
+  fetchTasks();
+},[])
+
  
  
   //Add Task
